39f8d5759fe651710da30cab2e4145cc9367c9f3
lpknv <nikitalipkanov@outlook.com>
2026-03-25 13:04:04 +0100

    update fonts, images, header style

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -11,11 +11,42 @@ $twig = new \Twig\Environment($loader, ['debug' => true]);
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 $routes = [
-  '/' => 'home',
-  '/kontakt' => 'contact',
-  '/baseballcamp' => 'baseballcamp',
+  '/' => [
+    'route' => 'home',
+    'data' => [
+      'upcoming_events' => [
+        [
+          'name' => 'Baseballcamp 2026',
+          'text' => 'Jeden Sonntag um 10 Uhr kommen wir zusammen, um Gott zu feiern und zu erleben.',
+        ]
+      ],
+      'weekly_events' => [
+        [
+          'name' => 'Gottesdienst, Sonntag 10:00 Uhr',
+          'text' => 'Jeden Sonntag um 10 Uhr kommen wir zusammen, um Gott zu feiern und zu erleben.',
+          'img' => 'godi'
+        ],
+        [
+          'name' => 'Kinderstunde',
+          'text' => 'Während des Gottesdienstes gibt es eine Kinderstunde für Kinder bis einschließlich der 4. Grundschulklasse.',
+          'img' => 'kinderstunde'
+        ],
+        [
+          'name' => 'Veranstaltungen',
+          'text' => 'Unser Veranstaltungsangebot ist vielseitig und abwechslungsreich – sowohl von uns als auch in Zusammenarbeit mit anderen. Ob online oder vor Ort, für Kinder oder Erwachsene, es ist für jeden etwas dabei',
+          'img' => 'events'
+        ],
+      ]
+    ],
+  ],
+  '/kontakt' => [
+    'route' => 'contact',
+  ],
+  '/baseballcamp' => [
+    'route' => 'baseballcamp',
+  ],
 ];
 
 if (key_exists($uri, $routes)) {
-  echo $twig->render("$routes[$uri].html.twig");
+  echo $twig->render(sprintf("%s.html.twig", $routes[$uri]['route']), $routes[$uri]['data'] ??= []);
 }
